2df8c16e3dd9e4df8c4db1559cfbc72830bc2dd9
Xssgh <127655685+Xssgh@users.noreply.github.com>
2023-06-05 13:51:20 +0900

    Add files via upload

---
+++ 뺄셈, 곱셈.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int a, b;
+	int result1, result2;
+	
+	//result1 = a - b;
+	//result2 = a * b;
+	
+	scanf("%d %d", &a, &b);
+	
+	result1 = a - b;
+	result2 = a * b;
+	
+	printf("%d %d\n", result1, result2);
+	 
+	return 0;
+}
