a0e1d499cbb99d68b36910a34a2fb942be4289d5
Xssgh <127655685+Xssgh@users.noreply.github.com>
2023-06-05 14:01:04 +0900

    Add files via upload

---
+++ 뺄셈, 곱셈.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int num1, num2, num3;
+	int result;
+
+	scanf("%d %d %d", &num1, &num2, &num3);	
+
+	result = num1*num2+num3;
+	
+	printf("num1xnum2+num3=%d", result);
+	
+	
+	 
+	return 0;
+}
